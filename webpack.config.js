931183e6b0e6f66190b3fd713a4a881e72e0e274
VWMAC <vincentkywong@gmail.com>
2020-03-21 21:07:21 -0700

    setting babel rules

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,14 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: ''
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_module/
+      }
+    ]
+  }
 };
