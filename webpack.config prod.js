5c63264bcb6fe4842b6a5c68737fd7563188efa5
VWMAC <vincentkywong@gmail.com>
2020-03-22 22:00:30 -0700

    production build

---
+++ webpack.config prod.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  devtool: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_module/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_module/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader', options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]_local_[hash:base64:5]'
+              }
+            }
+          },
+          {
+            loader: 'postcss-loader', options: {
+              ident: 'postcss',
+              plugins: () => [autoprefixer()]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+};
